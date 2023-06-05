c2857ad7b2541617a2ad031e2612d6590bf3a5c2
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-05 16:35:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
-
 function palindrome(str){
 
+	str = str.toLowerCase();
+	let string = str.split(" ").join('')
+	let arr=string.split('');
+	let i=0;j=arr.length-1;
+
+	while(i<j){
+		if(arr[i] !== arr[j]){
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
 }
 module.exports = palindrome
